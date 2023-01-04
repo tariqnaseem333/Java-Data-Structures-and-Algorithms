9b396b8cb20df4e4a9c38d99da6b9d880d94e20a
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-01-04 14:41:55 +0530

    Learn Array Basics

---
+++ Arrays and Strings/ArraysAndStrings/src/arraysIntroduction/BasicsOfArrays.java
@@ -0,0 +1,30 @@
+package arraysIntroduction;
+
+public class BasicsOfArrays {
+
+	public static void main(String[] args) {
+		
+//		int arr[] = new int[10];
+		int[] arr = new int[10];
+		arr[0] = 5;
+		arr[5] = 17;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[5]);
+		
+//		arr[-1] = 8;
+//		arr[10] = 13;
+	
+//		
+		System.out.println(arr[7]);
+		
+		
+		char[] cArray = new char[10];
+		double[] dArray = new double[10];
+		
+		System.out.println(cArray[0]);
+		System.out.println(dArray[0]);
+
+	}
+
+}
