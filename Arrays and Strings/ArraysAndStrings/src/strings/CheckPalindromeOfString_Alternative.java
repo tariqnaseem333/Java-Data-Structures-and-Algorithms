2c9f7fd86945f993cd8443b41e00925a67218750
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-02-23 23:20:01 +0530

    Solve CheckPalindromeOfString Problem of Strings

---
+++ Arrays and Strings/ArraysAndStrings/src/strings/CheckPalindromeOfString_Alternative.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.util.Scanner;
+
+public class CheckPalindromeOfString_Alternative {
+	
+	public static boolean checkPalindrome( String str ) {
+		boolean isPalindrome = false;
+
+		if( str.length() == 0 || str.length() == 1 ) {
+			return true;
+		}
+		
+		int start = 0;
+		int end = str.length() - 1;
+		while( start < end ) {
+			if( str.charAt(start) == str.charAt(end) ) {
+				isPalindrome = true;
+			} else {
+				isPalindrome = false;
+			}
+			start++;
+			end--;
+		}
+
+		return isPalindrome;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		boolean isPalindrome = checkPalindrome(str);
+		System.out.println(isPalindrome);
+
+	}
+
+}
